478086b301c7bb5b6b97cba5768eb53b9f43c2c4
Dmytro Korol <dkorol@gloriumtech.com>
2016-10-24 14:48:44 +0200

    workin auth

--- ng-app/src/app/login-page/login-page.component.ts
+++ ng-app/src/app/login-page/login-page.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter} from '@angular/core';
 import {WakandaClient} from 'wakanda-client';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
+  inputs: ['auth']
 })
 export class LoginPageComponent implements OnInit {
+  auth:boolean;
+
   public user:string;
   public pass:string;
   private _client;
+  onItemSelected: EventEmitter<string>;
 
   constructor() {
     this._client = new WakandaClient({});
   }
 
   doLogin(){
+    let self = this;
     this._client.directory.login(this.user, this.pass)
       .then(function () {
-
+        self.auth = true;
+        console.log('done');
       })
       .catch(function (e) {
         alert('Wrong user or password. Try again!')
